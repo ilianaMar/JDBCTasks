b52f8e62629629a58c7d0fbbac740b78855b2e63
Iliana Markova <iliana.markova@estafet.com>
2022-05-09 11:17:23 +0300

    fix test class name

--- src/test/java/org/estafet/FirstSuitTests.java
+++ src/test/java/org/estafet/FirstSuitTests.java
@@ -11,7 +11,7 @@ import org.estafet.helpers.DbHelper;
 import static org.junit.jupiter.api.Assertions.*;
 
 @DisplayName("First test suite")
-public class FirstTestSuite {
+public class FirstSuitTests {
     private static DbHelper dbHelper;
 
     static {
